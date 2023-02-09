b11fd2bbce780be8c56e1e834f61c3f5a0789ce4
Bruce Collie <brucecollie82@gmail.com>
2023-02-09 20:43:25 +0000

    Surgery on path

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -8,8 +8,10 @@ class Demo < Formula
   depends_on "llvm"          => :optional
 
   def install
-    puts ENV["PATH"]
+    old_path = ENV["PATH"]
+    ENV["PATH"].sub! "/usr/local/opt/llvm/bin:", ""
     system "stack", "setup"
+    ENV["PATH"] = old_path
     share.install "README.md"
   end
 
